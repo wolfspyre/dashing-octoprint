3993fe6befcd750aace3c86b8661c2e78591bafb
Wolf Noble <wolf@wolfspyre.com>
2015-02-23 04:07:08 -0600

    [DASHOCTO-1-create-octoprint-dashing-dashboard] disable warning statements

--- jobs/octoprint_snapshot.rb
+++ jobs/octoprint_snapshot.rb
@@ -11,7 +11,7 @@ octoConfMain = File.join(octoConfDir, "octoprint_defaults.yaml")
 octoConfOverride = File.join(octoConfDir, "octoprint_override.yaml")
 octoprint_config = YAML.load_file(octoConfMain)
 if File.exists?(octoConfOverride)
-  warn "OctoPrint: Merging Override"
+#warn "OctoPrint: Merging Override"
   octoprint_config = octoprint_config.merge(YAML.load_file(octoConfOverride))
 end
 
@@ -24,32 +24,33 @@ end
 @webcam_port=octoprint_config['octo_server_webcam_port']
 @webcam_frequency=octoprint_config['octo_server_webcam_poll_interval']
 @webcam_ssl_enable=octoprint_config['octo_server_webcam_ssl']
-warn "OctoPrint: #{octoConfDir}"
-warn "OctoPrint: #{octoConfMain}"
-warn "OctoPrint: #{octoConfOverride}"
-warn "OctoPrint: #{@api_port}"
-warn "OctoPrint: #{@current_file}"
-warn "OctoPrint: #{@last_file}"
-warn "OctoPrint: #{@api_key}"
-warn "OctoPrint: #{@octo_server}"
-warn "OctoPrint: #{@snapshot_url}"
-warn "OctoPrint: #{@webcam_port}"
-warn "OctoPrint: #{@webcam_frequency}"
-warn "OctoPrint: #{@webcam_ssl_enable}"
+#warn "OctoPrint: #{octoConfDir}"
+#warn "OctoPrint: #{octoConfMain}"
+#warn "OctoPrint: #{octoConfOverride}"
+#warn "OctoPrint: #{@api_port}"
+#warn "OctoPrint: #{@current_file}"
+#warn "OctoPrint: #{@last_file}"
+#warn "OctoPrint: #{@api_key}"
+#warn "OctoPrint: #{@octo_server}"
+#warn "OctoPrint: #{@snapshot_url}"
+#warn "OctoPrint: #{@webcam_port}"
+#warn "OctoPrint: #{@webcam_frequency}"
+#warn "OctoPrint: #{@webcam_ssl_enable}"
 def fetch_image(host,old_file,new_file, cam_port, cam_url, ssl_enable)
   begin
-    warn "OctoPrint: fetch_image:  #{host}"
-    warn "OctoPrint: fetch_image: #{old_file}"
-    warn "OctoPrint: fetch_image: #{new_file}"
-    warn "OctoPrint: fetch_image: #{cam_port}"
-    warn "OctoPrint: fetch_image: #{cam_url}"
+#warn "OctoPrint: fetch_image:  #{host}"
+#warn "OctoPrint: fetch_image: #{old_file}"
+#warn "OctoPrint: fetch_image: #{new_file}"
+#warn "OctoPrint: fetch_image: #{cam_port}"
+#warn "OctoPrint: fetch_image: #{cam_url}"
     if File.exists?(old_file)
-      warn "OctoPrint: fetch_image: deleting #{old_file}"
+#warn "OctoPrint: fetch_image: deleting #{old_file}"
       File.delete(old_file)
     end
     if File.exists?(new_file)
-      warn "OctoPrint: fetch_image: moving #{new_file} -> #{old_file}"
-      FileUtils.mv new_file, old_file, :verbose => true
+#warn "OctoPrint: fetch_image: moving #{new_file} -> #{old_file}"
+      #FileUtils.mv new_file, old_file, :verbose => true
+      FileUtils.mv new_file, old_file
     end
     uri = URI(cam_url)
     # Create client
@@ -68,7 +69,7 @@ def fetch_image(host,old_file,new_file, cam_port, cam_url, ssl_enable)
     #warn "OctoPrint: fetch_image: Response HTTP Status Code: #{res.code}"
     #warn "OctoPrint: fetch_image: Response HTTP Response Body: #{res.body}"
   rescue Exception => e
-    warn "OctoPrint: fetch_image: HTTP Request failed (#{e.message})"
+#warn "OctoPrint: fetch_image: HTTP Request failed (#{e.message})"
   end
   open(new_file, "wb") do |file|
     file.write(res.body)
@@ -86,16 +87,16 @@ def fetch_image(host,old_file,new_file, cam_port, cam_url, ssl_enable)
 #	new_file
 end
 def make_web_friendly(file)
-  warn "OctoPrint: make_web_friendly: #{file}"
+#warn "OctoPrint: make_web_friendly: #{file}"
   out = "/" + File.basename(File.dirname(file)) + "/" + File.basename(file)
-  warn "OctoPrint: make_web_friendly: #{out}"
+#warn "OctoPrint: make_web_friendly: #{out}"
   out
 end
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every "#{@webcam_frequency}s", first_in: 0 do
 	new_snapshot = fetch_image(@octo_server,@last_file,@current_file,@webcam_port,@snapshot_url,@webcam_ssl_enable)
 	if not File.exists?(@current_file)
-		warn "Failed to Get Camera Image"
+    warn "Failed to Get Camera Image"
 	end
 	send_event('octoprint_snapshot', octo_image: make_web_friendly(new_snapshot))
 	sleep(@webcam_frequency.to_i)
