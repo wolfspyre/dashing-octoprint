0c00d5d3c0abb3c7b5222fb3a1a7836c0ecaf962
Wolf Noble <wolf@wolfspyre.com>
2015-03-02 02:58:05 -0600

    [DASHOCTO-3] iterate on graph job. limit data arrays to a depth listed in conf file

--- jobs/octoprint_graphs.rb
+++ jobs/octoprint_graphs.rb
@@ -29,19 +29,20 @@ end
 @tool0_color_actual=octoprint_config['octo_server_printer_tool_0_temp_graph_color_actual']
 @tool0_color_target=octoprint_config['octo_server_printer_tool_0_temp_graph_color_target']
 @tool0_graph_enable=octoprint_config['octo_server_printer_tool_0_temp_graph_enable']
+@graph_depth=octoprint_config['octo_server_graph_depth']
 
-warn "OctoPrint: api_key: #{api_key}"
-warn "OctoPrint: api_port: #{api_port}"
-warn "OctoPrint: api_ssl_enable: #{api_ssl_enable}"
-warn "OctoPrint: bed_color_actual: #{bed_color_actual}"
-warn "OctoPrint: bed_color_target: #{bed_color_target}"
-warn "OctoPrint: bed_graph_enable: #{bed_graph_enable}"
-warn "OctoPrint: frequency: #{frequency}"
-warn "OctoPrint: octo_server: #{octo_server}"
-warn "OctoPrint: printer_endpoint: #{printer_endpoint}"
-warn "OctoPrint: tool0_color_actual: #{tool0_color_actual}"
-warn "OctoPrint: tool0_color_target: #{tool0_color_target}"
-warn "OctoPrint: tool0_graph_enable: #{tool0_graph_enable}"
+warn "OctoPrint: api_key: #{@api_key}"
+warn "OctoPrint: api_port: #{@api_port}"
+warn "OctoPrint: api_ssl_enable: #{@api_ssl_enable}"
+warn "OctoPrint: bed_color_actual: #{@bed_color_actual}"
+warn "OctoPrint: bed_color_target: #{@bed_color_target}"
+warn "OctoPrint: bed_graph_enable: #{@bed_graph_enable}"
+warn "OctoPrint: frequency: #{@frequency}"
+warn "OctoPrint: octo_server: #{@octo_server}"
+warn "OctoPrint: printer_endpoint: #{@printer_endpoint}"
+warn "OctoPrint: tool0_color_actual: #{@tool0_color_actual}"
+warn "OctoPrint: tool0_color_target: #{@tool0_color_target}"
+warn "OctoPrint: tool0_graph_enable: #{@tool0_graph_enable}"
 
 #see 'Passing Data' section of the rickshawgraphs plugin
 #
@@ -109,8 +110,8 @@ end
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every "#{@frequency}s", first_in: 0 do
 	data = getOctoPrintStatus(@octo_server,@api_port,@api_key,@printer_endpoint,@api_ssl_enable)
-  warn "OctoPrint: #{data}"
-  time = Time.new.to_datetime.to_i
+#  warn "OctoPrint: #{data}"
+  time = Time.now.to_i
   if data
     if @bed_graph_enable
       bed_actual=data['temps']['bed']['actual'].to_i
@@ -119,6 +120,8 @@ SCHEDULER.every "#{@frequency}s", first_in: 0 do
       bed_target_now=[bed_target,time]
       bed_actual_datapoints<<bed_actual_now
       bed_target_datapoints<<bed_target_now
+      bed_actual_datapoints=bed_actual_datapoints.take(@graph_depth)
+      bed_target_datapoints=bed_target_datapoints.take(@graph_depth)
       bed_graphite = [
         {
           target: "actual_temp", datapoints: bed_actual_datapoints
@@ -128,7 +131,7 @@ SCHEDULER.every "#{@frequency}s", first_in: 0 do
         }
       ]
       warn "OctoPrint: bed_graphite data: #{bed_graphite}"
-      send_event('octoprint_bed_graph', bed_graphite)
+      send_event('octoprint_bed_graph', series: bed_graphite)
       sleep 1
     end
     if @tool0_graph_enable
@@ -138,6 +141,8 @@ SCHEDULER.every "#{@frequency}s", first_in: 0 do
       tool0_target_now=[tool0_target,time]
       tool0_actual_datapoints<<tool0_actual_now
       tool0_target_datapoints<<tool0_target_now
+      tool0_actual_datapoints=tool0_actual_datapoints.take(@graph_depth)
+      tool0_target_datapoints=tool0_target_datapoints.take(@graph_depth)
       tool0_graphite = [
         {
           target: "actual_temp", datapoints: tool0_actual_datapoints
