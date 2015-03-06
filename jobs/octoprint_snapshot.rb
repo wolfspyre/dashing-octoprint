6fe8af8d942d4a0f8f62962942054413956c1949
Wolf Noble <wolf@wolfspyre.com>
2015-03-06 05:03:59 -0600

    [DASHOCTO-4] try to address image not updating

--- jobs/octoprint_snapshot.rb
+++ jobs/octoprint_snapshot.rb
@@ -98,5 +98,7 @@ SCHEDULER.every "#{@webcam_frequency}s", first_in: 0 do
 	if not File.exists?(@current_file)
     warn "Failed to Get Camera Image"
 	end
+  send_event('octoprint_snapshot', {"octo-image": @last_file})
+  sleep 1
 	send_event('octoprint_snapshot', {"octo-image": make_web_friendly(new_snapshot)})
 end
